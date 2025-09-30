c34b15d912cb76a2d44c271d995c1ec145ece4b3
maximiliano nadalin <maximilianoenadalin@gmail.com>
2025-09-30 08:09:58 +0200

    models category

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,9 +1,30 @@
 from django.db import models
 from django.utils import timezone
+import uuid
+
 # Create your models here.
 
 def thumbnail_blog(instance,filename):
     return "blog/{0}/{1}".format(instance.title,filename)
+
+def thumbnail_blog_category(instance,filename):
+    return "blog_category/{0}/{1}".format(instance.name,filename)
+
+
+class Category(models.Model):
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
+    parent = models.ForeignKey(self,related_name="children",on_delete=models.CASCADE,blank=True,null=True)
+    name = models.models.CharField(max_length=50)
+    title = models.models.CharField(max_length=50)
+    description = models.TextField()
+    thumbnail = models.ImageField(upload_to=thumbnail_blog_category)
+    slug = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+       
 
 
 class Post(models.Model):
@@ -17,6 +38,7 @@ class Post(models.Model):
         ("published", "Published")
     )
     
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
     tittle = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
     content = models.TextField()
@@ -31,9 +53,11 @@ class Post(models.Model):
     objects = models.Manager()
     postobject = PostObjects()
     
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    
     class Meta:
         ordering = ("-published")
         
     def __str__(self):
         return self.tittle
-    
+
